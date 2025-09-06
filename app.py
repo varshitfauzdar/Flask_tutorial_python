435dd6331f03b24b39706d4a17133810018466cb
varshitfauzdar <varshitfauzdar2004@gmail.com>
2025-09-06 23:53:54 +0530

    2nd update

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def helloworld():
     return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/about')
+def about():
+    return "hyy I am Varshit Fauzdar"
 
-# check the port is running
+if __name__ == '__main__':
+    app.run(debug=True, port = 8000)
