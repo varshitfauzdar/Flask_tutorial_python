3e0456f337260bf89c57b00c3c1a58241d2f9316
varshitfauzdar <varshitfauzdar2004@gmail.com>
2025-09-07 22:30:51 +0530

    flask complete tutorial

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for,jsonify
+ 
 
 app = Flask(__name__)
 
@@ -18,10 +19,41 @@ def success(score):
 def fail(score):
     return "Aww lele babu fail hogya...kitne aae " + str(score)
 
-@app.route('/form',methods=['GET','POST'])
+@app.route('/login',methods=['GET','POST'])
 def form():
     if request.method == 'GET':
-        return render_template('form.html')
+        return render_template('login_page.html')
+
+
+@app.route('/form',methods=['GET','POST'])
+def form_tut():
+    if request.method == 'GET':
+        return render_template('form_tut.html')
+    else:
+        maths = float(request.form['maths'])
+        science = float(request.form['science'])
+        history = float(request.form['history'])
+
+        average = (maths + science + history) / 3
+
+        result = ""
+        if average >= 50:
+            result = "success"
+        else:
+            result = "fail"
+
+        return redirect(url_for(result,score=average))
+        # return render_template('form_tut.html',score=average)
+
+@app.route('/api',methods=['POST'])     #use postman to test the api
+def calculate_sum():
+    data = request.get_json()
+    a_val = float(dict(data)['a'])
+    b_val = float(dict(data)['b'])
+
+    return jsonify({'sum':a_val+b_val})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port = 8000)
