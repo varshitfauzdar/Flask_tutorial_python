a883e37766c4fb47f189db80245ee2cce2e93cb7
varshitfauzdar <varshitfauzdar2004@gmail.com>
2025-09-07 16:46:00 +0530

    adding html form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -16,7 +16,12 @@ def success(score):
 
 @app.route('/fail/<int:score>')
 def fail(score):
-    return "Aww lele babu fail hogya...kitne aae " + str(score)  
+    return "Aww lele babu fail hogya...kitne aae " + str(score)
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method == 'GET':
+        return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port = 8000)
